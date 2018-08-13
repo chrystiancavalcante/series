d8e7bb6725c3b8dd2c751d0b41d89a9246348361
chrystian cavalcante <chrystiancavalcante@gmail.com>
2018-08-13 15:35:30 -0300

    filtro por series

--- src/Series.js
+++ src/Series.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class Series extends Component {
     render(){
-        return <section id="intro" className="intro-section"><h1>Series</h1></section>
+        return <section id="intro" className="intro-section"><h1>Series: {this.props.match.params.genre}</h1></section>
     }
 }
 
